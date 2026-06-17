e1e4714f8d22f793b0f768d4ead322df1e0758cc
amalraj4 <amalraj@locatenow.ai>
2026-06-17 18:24:26 +0530

    Initial commit

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: ['react-native-gesture-handler/jestSetup'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native|@react-navigation|react-native-gesture-handler|react-native-safe-area-context|react-native-screens|react-native-vector-icons|react-native-camera-kit)/)',
   ],
